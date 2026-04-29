129206d19febb9bfa6cfd98cc899137846439f4e
mVazquez98-hub <tinchovazquez45@gmail.com>
2026-04-29 12:48:35 -0300

    Inicio, menu principal y charsets

--- src/obligatorio/Main.java
+++ src/obligatorio/Main.java
@@ -1,9 +1,17 @@
 package obligatorio;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Inicio MatrixGameTester");
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8.name()));
+        System.out.println("Trabajo desarrollado por: Martín Vázquez - 180210");
+        System.out.println("MatrixGameTester - Inicio");
+
+        Sistema sistema = new Sistema();
+        sistema.iniciar();
     }
 
 }
--- src/obligatorio/Sistema.java
+++ src/obligatorio/Sistema.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package obligatorio;
 
-/**
- *
- * @author tinch
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Sistema {
-    
+
+    private Tablero tablero;
+    private ArrayList<Tester> testers;
+    private ArrayList<Testeo> testeos;
+    private Scanner in;
+
+    public Sistema() {
+        this.tablero = new Tablero();
+        this.testers = new ArrayList<Tester>();
+        this.testeos = new ArrayList<Testeo>();
+        this.in = new Scanner(System.in);
+
+    }
+
+    public void iniciar() {
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("f")) {
+            try {
+                mostrarMenu();
+                opcion = in.nextLine();
+                if (opcion.equalsIgnoreCase("a")) {
+                    registrarTester();
+                } else if (opcion.equalsIgnoreCase("b")) {
+                    registrarMatrizActual();
+                } else if (opcion.equalsIgnoreCase("c")) {
+                    registrarTesteo();
+                } else if (opcion.equalsIgnoreCase("d")) {
+                    consultarTesters();
+                } else if (opcion.equalsIgnoreCase("e")) {
+                    mostrarEstadisticas();
+                } else if (opcion.equalsIgnoreCase("f")) {
+                    System.out.println("Fin del programa.");
+                } else {
+                    System.out.println("Opción inválida.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error, ingrese una opción válida");
+
+            }
+
+        }
+
+    }
+
+    public void mostrarMenu() {
+        System.out.println("a) Registrar tester");
+        System.out.println("b) Registrar matriz actual del juego");
+        System.out.println("c) Registrar testeo");
+        System.out.println("d) Consulta de testers");
+        System.out.println("e) Estadísticas");
+        System.out.println("f) Terminar");
+    }
+
+    public void registrarTester() {
+
+    }
+    public void registrarMatrizActual(){
+        
+    }
+    public void registrarTesteo(){
+        
+    }
+    public void consultarTesters(){
+        
+    }
+    public void mostrarEstadisticas(){
+        
+    }
+
+
 }
